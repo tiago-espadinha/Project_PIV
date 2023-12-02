8dce2dc24592a78562e55fc9e56e3d81063780cd
espadela <tiago.espadinha.simoes@gmail.com>
2023-12-02 22:17:33 +0000

    Added feature extraction

--- process_video.py
+++ process_video.py
@@ -1,6 +1,5 @@
 import numpy as np
 import scipy
-import sys
 from configparser import ConfigParser
 import cv2
 import time
@@ -14,32 +13,50 @@ def main():
     # # Opens the config file
     # config_file = sys.argv[1]
 
-
     config_file = 'conf_file.cfg'
+
     config = ConfigParser()
     config.read(config_file)
     config = config['DEFAULT']
 
-    print("Config file: ", config['videos'])
+    # Load video
     video = cv2.VideoCapture(config['videos'])
-
+    sift = cv2.SIFT_create()
+    video_data = []
+    
     while True:
         ret, frame = video.read()
         if not ret:
             break
-        cv2.imshow('frame', frame)
+
+        # Identify frame keypoints and descriptors
+        keypoints, descriptors = sift.detectAndCompute(frame, None)
+        
+        # Display the resulting frame
+        img = cv2.drawKeypoints(frame, keypoints, frame)
+        cv2.imshow('frame', img)
+
+        # Append frame data to matlab array
+        keypoints = np.float32([keypoints[m].pt for m in range(len(keypoints))]).reshape(-1,2)
+        descriptors = np.float32(descriptors)
+        frame_data = {'keypoints': keypoints, 'descriptors': descriptors}
+        video_data.append(frame_data)
+
         time.sleep(1/30)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-    # What should be outputed
-    f=scipy.io.loadmat('Project_PIV/specs/surf_features.mat')
+    # Prepare data for matlab
+    compiled_data = np.array([(frame_data['keypoints'], frame_data['descriptors']) for frame_data in video_data], dtype=object)
+    mdic = {"features": compiled_data}
+    scipy.io.savemat('features.mat', mdic)
+
+    # Check output format
+    f=scipy.io.loadmat('features.mat')
     feat = f['features']
     print("Feature")
     print(" -> type: ", type(feat))
     print(" -> shape: ", feat.shape)
-    print("Keypoint")
-    print(" -> shape: ", feat[0,2].shape)
 
 if __name__ == '__main__':
     main()
