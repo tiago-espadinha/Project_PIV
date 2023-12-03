2cb14a13e4854e4b50be419cff3bbd9a9e46c6c0
Tomás Santos <129119055+SteveMcjones2103@users.noreply.github.com>
2023-12-03 20:10:01 +0000

    Update get_keypoints.py

--- get_keypoints.py
+++ get_keypoints.py
@@ -1,10 +1,5 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import scipy.io
 from random import randrange
-from PIL import Image
-import plotly.graph_objects as go
-import plotly.offline as pyo
 import cv2
 
 def click_event(event, x, y, flags, params):
@@ -13,22 +8,9 @@ def click_event(event, x, y, flags, params):
         cv2.imshow(params['window_name'], params['image'])
         params['points'].append((x, y))
 
-def compute_homography(points1, points2):
-    A = []
-    for i in range(len(points1)):
-        x, y = points1[i][0], points1[i][1]
-        xp, yp = points2[i][0], points2[i][1]
-        A.append([-x, -y, -1, 0, 0, 0, x*xp, y*xp, xp])
-        A.append([0, 0, 0, -x, -y, -1, x*yp, y*yp, yp])
-    A = np.array(A)
-    U, S, Vh = np.linalg.svd(A)
-    L = Vh[-1,:] / Vh[-1,-1]
-    H = L.reshape(3, 3)
-    return H
-
 # Load images
-image1 = cv2.imread('image_examples\parede1.jpg', cv2.IMREAD_COLOR)
-image2 = cv2.imread('image_examples\parede2.jpg', cv2.IMREAD_COLOR)
+image1 = cv2.imread('processed_videos/trymefirst/frames/frame_0000.jpg', cv2.IMREAD_COLOR)
+image2 = cv2.imread('processed_videos/trymefirst/frames/frame_1355.jpg', cv2.IMREAD_COLOR)
 
 # Initialize lists to store points
 points_image1 = []
@@ -51,10 +33,10 @@ if len(points_image1) != len(points_image2) or len(points_image1) < 4:
     raise ValueError("Different number of points selected in images or not enough points")
 
 # Convert points to NumPy arrays
-points1 = np.array(points_image1, dtype='float32')
-points2 = np.array(points_image2, dtype='float32')
+points1 = np.array(points_image1, dtype='int32')
+points2 = np.array(points_image2, dtype='int32')
 
-fd = open('keypoints.txt', mode='w')
-fd.write("Points 1: \n", str(points1))
-fd.write("\nPoints 2: \n", str(points2))
-fd.close
+with open('keypoints.txt', mode='w') as fd:
+    fd.write("Points 1: \n" + str(points1))
+    fd.write("\nPoints 2: \n" + str(points2))
+# fd.close
