0c2e8b31b9f2f7315dfdddc942cdb38865f439db
FilipeTendeiro <152648175+FilipeTendeiro@users.noreply.github.com>
2023-12-18 16:59:30 +0000

    compute_transforms.py

--- compute_transforms.py
+++ compute_transforms.py
@@ -59,41 +59,86 @@ def find_best_homography(points1, points2, P=0.99, p=0.5, n_samples=4):
         # Build the design matrix
         H = compute_homography(sampled_points1, sampled_points2)
         H_normalized = H / H[2, 2]
-        # H2 = compute_homography(sampled_points2, sampled_points1)
-        # H2_normalized = H2 / H2[2, 2]
 
         points1 = np.hstack((points1, np.ones((len(points1), 1))))
         points1_transformed = np.copy(points1)
-        # points2 = np.hstack((points2, np.ones((len(points2), 1))))
-        # points2_transformed = np.copy(points2)
 
         for j in range(len(points1)):
             points1_transformed[j] = H_normalized @ points1[j]
             points1_transformed[j] /= points1_transformed[j][2]
 
-        # for j in range(len(points2)):
-        #     points2_transformed[j] = H2_normalized @ points2[j]
-        #     points2_transformed[j] /= points2_transformed[j][2]
-
         points1_transformed = points1_transformed[:, :2]
-
+        inliers = 0
+        for j in range(len(points1)):
+            # if the distance of the matching points is lower than a certain threshold after projecting a 
+            # point from a frame to another than they are considered as inliers
+            if np.mean((points2[j] - points1_transformed[j])**2) < 3.0:
+                inliers += 1
+        error = np.mean((points2 - points1_transformed)**2)
+            
         if i==0:
             best_H = H_normalized
-            min_error = np.mean((points2 - points1_transformed)**2)
+            min_error = error
+            max_inliers = inliers
         else:
-            error = np.mean((points2 - points1_transformed)**2)
-            if error < min_error:
+            if inliers > max_inliers:
                 best_H = H_normalized
                 min_error = error
+                max_inliers = inliers
         points1 = points1[:, :2]
-        # points2 = points2[:, :2]
+    print(f"Min Error: {min_error} with {max_inliers} inliers")
+    return best_H, min_error
 
-    return best_H
+def match_and_compute(features, frame_1, frame_2, width, height):
+    keypoints_1 = features[frame_1][0]
+    descriptors_1 = features[frame_1][1]
+    keypoints_2 = features[frame_2][0]
+    descriptors_2 = features[frame_2][1]
+    
+    # Create a Brute Force Matcher
+    bf = cv2.BFMatcher()
+    
+    # Match descriptors using KNN (k-nearest neighbors)
+    matches = bf.knnMatch(descriptors_1, descriptors_2, k=2)
+    
+    # Apply ratio test to get good matches
+    good_matches = []
+    for m, n in matches:
+        if m.distance < 0.5 * n.distance:
+            good_matches.append(m)
+            
+    print(f"Number of good matches between frames {frame_2} and {frame_1}: {len(good_matches)}")
+    
+    if len(good_matches) < 20:
+        H1 = match_and_compute(features, int((frame_2+frame_1)/2), frame_2, width, height)
+        H2 = match_and_compute(features, frame_1, int((frame_2+frame_1)/2), width, height)
+        return H1 @ H2
+
+    points_1 = np.float32([keypoints_1[m.queryIdx] for m in good_matches]).reshape(-1, 2)
+    points_2 = np.float32([keypoints_2[m.trainIdx] for m in good_matches]).reshape(-1, 2)
+    
+    H, error = find_best_homography(points_1, points_2, P=0.99, p=0.5, n_samples=4)
+    
+    return H
     
 def main():
-
+    """ Drone dataset:
+    frames_directory = 'processed_videos/trymefirst/frames/'
+    """
+    """ Tesla dataset:
+    frames_directory =  'processed_videos/TeslaVC_carreira/back_frames/'
+    """
+    frames_directory = 'processed_videos/trymefirst/frames/'
+    
+    # keypoints1 = feat[frame1_number][0]
+    # descriptors1 = feat[frame1_number][1]
+    # keypoints2 = feat[frame2_number][0]
+    # descriptors2 = feat[frame2_number][1]
+    
+    # Get data from the configuration file
+    config = load_config('part1.cfg', 'DEFAULT')
     # what should be outputed
-    f=scipy.io.loadmat('features.mat')
+    f=scipy.io.loadmat(config['keypoints_out'])
     feat = f['features']
     print("Feature")
     print(" -> type: ", type(feat))
@@ -101,75 +146,41 @@ def main():
     print("Keypoint")
     print(" -> shape: ", feat[0].shape)
     
-    frames_directory = 'processed_videos/trymefirst/frames/'
-    
-    # keypoints1 = feat[0][0]
-    # descriptors1 = feat[0][1]
-    # keypoints2 = feat[1355][0]
-    # descriptors2 = feat[1355][1]
-    
-    # Get data from the configuration file
-    config = load_config('conf_file.cfg', 'DEFAULT')
     transform_type, transform_scope = parse_transforms(config)
     image_map = cv2.imread(config['image_map'], cv2.IMREAD_COLOR)
     height, width = image_map.shape[:2]
-    label, map_points, frame_number, frame_points = parse_matches(config)
-    
-    # Get Features from the map
-    sift = cv2.SIFT.create()
-    keypoints_map, descriptors_map = sift.detectAndCompute(image_map, None)
-    keypoints_map = np.float32([keypoints_map[m].pt for m in range(len(keypoints_map))]).reshape(-1,2)
+    _, map_points, keyframe_number, keyframe_points = parse_matches(config)
     
     if transform_type == 'homography':
-        H_map = compute_homography(frame_points, map_points)
-        image_frame = cv2.imread(f'{frames_directory}frame_{frame_number:04d}.jpg', cv2.IMREAD_COLOR)
-        warped_image_1 = cv2.warpPerspective(image_frame, H_map, (width, height))
-        
-        # Create a 2x2 subplot grid
-        fig, axs = plt.subplots(2, figsize=(10, 10))
-        
-        # Original left image
-        axs[0].imshow(cv2.cvtColor(image_map, cv2.COLOR_BGR2RGB))
-        axs[0].set_title('Map Image')
-        
-        # Original right image
-        axs[1].imshow(cv2.cvtColor(warped_image_1, cv2.COLOR_BGR2RGB))
-        axs[1].set_title('Warped Frame Image')
-        
-        # Adjust layout for better spacing
-        plt.tight_layout()
-        
-        # Display the plot
-        plt.show() 
+        H_map = compute_homography(keyframe_points, map_points)
+        image_keyframe = cv2.imread(f'{frames_directory}frame_{keyframe_number:04d}.jpg', cv2.IMREAD_COLOR)
         
         if transform_scope == 'map':    # Compute all homographies between each frame and the map
             for i in range(len(feat)):
-                keypoints1 = feat[i][0]
-                descriptors1 = feat[i][1]
                 frame_filename = f'{frames_directory}frame_{i:04d}.jpg'
-                image1 = cv2.imread(frame_filename, cv2.IMREAD_COLOR)
+                image = cv2.imread(frame_filename, cv2.IMREAD_COLOR)
                 
-                # Create a Brute Force Matcher
-                bf = cv2.BFMatcher()
+                H = match_and_compute(feat, i, keyframe_number, width, height)     
+                warped_image = cv2.warpPerspective(image, H, (width, height))  
+                warped_image_to_map = cv2.warpPerspective(warped_image, H_map, (width, height))
                 
-                # Match descriptors using KNN (k-nearest neighbors)
-                matches = bf.knnMatch(descriptors1, descriptors_map, k=2)
+                fig, axs = plt.subplots(2, 2, figsize=(10, 10))
                 
-                # Apply ratio test to get good matches
-                good_matches = []
-                for m, n in matches:
-                    if m.distance < 0.5 * n.distance:
-                        good_matches.append(m)
+                axs[0][0].imshow(cv2.cvtColor(image_map, cv2.COLOR_BGR2RGB))
+                axs[0][0].set_title('Map Image')
                 
-                # Get corresponding points
-                points1 = np.float32([keypoints1[m.queryIdx] for m in good_matches]).reshape(-1, 2)
-                points2 = np.float32([keypoints_map[m.trainIdx] for m in good_matches]).reshape(-1, 2)
+                axs[0][1].imshow(cv2.cvtColor(warped_image_to_map, cv2.COLOR_BGR2RGB))
+                axs[0][1].set_title(f'Warped frame {i} to map')
                 
-                H = find_best_homography(points1, points2, P=0.99, p=0.5, n_samples=4)
-                warped_image_1 = cv2.warpPerspective(image1, H, (width, height))
+                axs[1][0].imshow(cv2.cvtColor(warped_image, cv2.COLOR_BGR2RGB))
+                axs[1][0].set_title(f'Warped frame {i} to keyframe{keyframe_number}')
                 
+                axs[1][1].imshow(cv2.cvtColor(image_keyframe, cv2.COLOR_BGR2RGB))
+                axs[1][1].set_title(f'Keyframe {keyframe_number}')
                 
-
+                plt.tight_layout()
+                plt.show() 
+                
 if __name__ == '__main__':
     main()
     
