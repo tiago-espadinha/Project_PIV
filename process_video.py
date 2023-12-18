ad9cb0056866a60f333a1bf433df8e23922150c4
FilipeTendeiro <152648175+FilipeTendeiro@users.noreply.github.com>
2023-12-18 16:58:45 +0000

    process_video.py

--- process_video.py
+++ process_video.py
@@ -3,9 +3,10 @@ import scipy
 from configparser import ConfigParser
 import cv2
 import time
+import sys
 
 def main():
-    # Use config file as input argument
+    # # Use config file as input argument
     # if len(sys.argv) != 2:
     #     print("Usage: python process_video.py <config_file>")
     #     sys.exit(1)
@@ -13,7 +14,7 @@ def main():
     # # Opens the config file
     # config_file = sys.argv[1]
 
-    config_file = 'conf_file.cfg'
+    config_file = 'part1.cfg'
 
     config = ConfigParser()
     config.read(config_file)
@@ -21,8 +22,14 @@ def main():
 
     # Load video
     video = cv2.VideoCapture(config['videos'])
-    sift = cv2.SIFT_create()
+    sift = cv2.SIFT_create(nfeatures = 0)
     video_data = []
+    
+    # Set the undersampling factor
+    undersampling_factor = 30  # Change this to the desired factor
+    
+    # Initialize a frame counter
+    frame_count = 0
     
     while True:
         ret, frame = video.read()
@@ -31,28 +38,32 @@ def main():
 
         # Identify frame keypoints and descriptors
         keypoints, descriptors = sift.detectAndCompute(frame, None)
-        
-        # Display the resulting frame
-        img = cv2.drawKeypoints(frame, keypoints, frame)
-        cv2.imshow('frame', img)
+        if frame_count % undersampling_factor == 0:
+            # Display the resulting frame
+            img = cv2.drawKeypoints(frame, keypoints, frame)
+            cv2.imshow('frame', img)
 
-        # Append frame data to matlab array
-        keypoints = np.float32([keypoints[m].pt for m in range(len(keypoints))]).reshape(-1,2)
-        descriptors = np.float32(descriptors)
-        frame_data = {'keypoints': keypoints, 'descriptors': descriptors}
-        video_data.append(frame_data)
+            # Append frame data to matlab array
+            keypoints = np.float32([keypoints[m].pt for m in range(len(keypoints))]).reshape(-1,2)
+            descriptors = np.float32(descriptors)
+            frame_data = {'keypoints': keypoints, 'descriptors': descriptors}
+            video_data.append(frame_data)
+            
+        # Increment the frame counter
+        frame_count += 1
 
-        time.sleep(1/30)
+        # time.sleep(1/30)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+        
 
     # Prepare data for matlab
     compiled_data = np.array([(frame_data['keypoints'], frame_data['descriptors']) for frame_data in video_data], dtype=object)
     mdic = {"features": compiled_data}
-    scipy.io.savemat('features.mat', mdic)
+    scipy.io.savemat(config['keypoints_out'], mdic)
 
     # Check output format
-    f=scipy.io.loadmat('features.mat')
+    f=scipy.io.loadmat(config['keypoints_out'])
     feat = f['features']
     print("Feature")
     print(" -> type: ", type(feat))
